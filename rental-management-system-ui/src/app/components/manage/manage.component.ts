ac7ed8eda8f84c9d5ef1a5ac402e3113698e91d5
ManujaNethranga <manujawimalaweera@gmail.com>
2024-07-26 17:16:06 +0530

    Employee and items Save Components Createdgit add .

--- rental-management-system-ui/src/app/components/manage/manage.component.ts
+++ rental-management-system-ui/src/app/components/manage/manage.component.ts
@@ -1,12 +1,24 @@
+import { NgFor } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-manage',
   standalone: true,
-  imports: [],
+  imports: [HttpClientModule,NgFor,FormsModule],
   templateUrl: './manage.component.html',
   styleUrl: './manage.component.css'
 })
 export class ManageComponent {
 
+  constructor(private http:HttpClient){}
+
+  public customerList : any ;
+
+  loadCustomers(){
+    this.http.get("http://localhost/8080/items").subscribe(res=>{
+      this.customerList= res;
+    })
+  }
 }
--- rental-management-system-ui/src/app/components/register/register.component.ts
+++ rental-management-system-ui/src/app/components/register/register.component.ts
@@ -1,12 +1,57 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [FormsModule,CommonModule,HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+
+  public customer = {
+    "name" : undefined,
+    "city" : undefined,
+    "contact" : undefined
+  }
+
+  
+
+  constructor(private http:HttpClient){}
+  saveEmployee(){
+    this.http.post("http://localhost:8080/customer",this.customer).subscribe(
+      (data)=>{
+        Swal.fire({
+          title: "Good job!",
+          text: "Employee Entered Successfully!",
+          icon: "success"
+        });
+      }
+    )
+
+  }
+
+  public item = {
+    "name" : undefined,
+    "rentalPerDay" : undefined,
+    "finePerDay" : undefined,
+    "availability" : undefined
+  }
+
+  saveItem(){
+    this.http.post("http://localhost:8080/items",this.item).subscribe(
+      (data)=>{
+        Swal.fire({
+          title: "Good job!",
+          text: "Employee Entered Successfully!",
+          icon: "success"
+        });
+      }
+    )
+  }
 
 }
